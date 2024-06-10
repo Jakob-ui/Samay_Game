6c536f2261e6b818e33feea3b231092049c9ac26
Jakob-ui <Jakoblaschober@gmail.com>
2024-06-10 21:03:22 +0200

    Sounds added

--- Samay_Game/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Samay_Game/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -11,8 +11,56 @@
 
 namespace AK
 {
+    namespace EVENTS
+    {
+        static const AkUniqueID PAUSE_JUNGLE_AMBIENCE = 3281489922U;
+        static const AkUniqueID PLAY_BUTTONS = 2461875365U;
+        static const AkUniqueID PLAY_CAVEDRIPS = 4118844927U;
+        static const AkUniqueID PLAY_FALLING = 913957623U;
+        static const AkUniqueID PLAY_FIRECRACKLING = 2550218880U;
+        static const AkUniqueID PLAY_FOOTSTEPS = 3854155799U;
+        static const AkUniqueID PLAY_HITTINGGROUND = 1168366424U;
+        static const AkUniqueID PLAY_HOURGLASS = 1370920248U;
+        static const AkUniqueID PLAY_JUMP = 3689126666U;
+        static const AkUniqueID PLAY_JUNGLE_AMBIENCE = 2245623692U;
+        static const AkUniqueID PLAY_LEVER = 3196500978U;
+        static const AkUniqueID PLAY_ROCKRUMBLE = 3120591130U;
+        static const AkUniqueID PLAY_TIMESTOP = 110520045U;
+        static const AkUniqueID PLAY_WATERFLOW = 518242827U;
+        static const AkUniqueID TIMEBAR = 3776642413U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace LEVER_STATE
+        {
+            static const AkUniqueID GROUP = 627643701U;
+
+            namespace STATE
+            {
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID OFF = 930712164U;
+                static const AkUniqueID ON = 1651971902U;
+            } // namespace STATE
+        } // namespace LEVER_STATE
+
+    } // namespace STATES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID SPEED = 640949982U;
+        static const AkUniqueID TIMEBAR = 3776642413U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAIN = 3161908922U;
+    } // namespace BANKS
+
     namespace BUSSES
     {
+        static const AkUniqueID CAVEAUDIOBUS = 2705837510U;
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
     } // namespace BUSSES
 
