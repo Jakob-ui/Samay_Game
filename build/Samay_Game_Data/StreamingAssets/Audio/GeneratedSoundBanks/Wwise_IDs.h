5895c773c199605b989684a99fe018de22c2929b
Jakob-ui <Jakoblaschober@gmail.com>
2024-06-18 12:33:24 +0200

    upload build

---
+++ build/Samay_Game_Data/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,73 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PAUSE_JUNGLE_AMBIENCE = 3281489922U;
+        static const AkUniqueID PLAY_BUTTONS = 2461875365U;
+        static const AkUniqueID PLAY_CAVEDRIPS = 4118844927U;
+        static const AkUniqueID PLAY_FALLING = 913957623U;
+        static const AkUniqueID PLAY_FIRECRACKLING = 2550218880U;
+        static const AkUniqueID PLAY_FOOTSTEPS = 3854155799U;
+        static const AkUniqueID PLAY_HITTINGGROUND = 1168366424U;
+        static const AkUniqueID PLAY_HOURGLASS = 1370920248U;
+        static const AkUniqueID PLAY_JUMP = 3689126666U;
+        static const AkUniqueID PLAY_JUNGLE_AMBIENCE = 2245623692U;
+        static const AkUniqueID PLAY_LEVER_FALSE = 1858556990U;
+        static const AkUniqueID PLAY_LEVER_TRUE = 4279771213U;
+        static const AkUniqueID PLAY_PENDULUM = 2958693236U;
+        static const AkUniqueID PLAY_PRESSUREPLATEDOWN = 912371233U;
+        static const AkUniqueID PLAY_PRESSUREPLATEUP = 532613478U;
+        static const AkUniqueID PLAY_ROCKRUMBLE = 3120591130U;
+        static const AkUniqueID PLAY_SAMAY_CUTSCENE_MUSIC = 2377306268U;
+        static const AkUniqueID PLAY_SPIKES = 996860547U;
+        static const AkUniqueID PLAY_STONE_DOOR_CLOSE = 820821253U;
+        static const AkUniqueID PLAY_STONE_DOOR_OPEN = 224478607U;
+        static const AkUniqueID PLAY_SUCCESS = 3943614907U;
+        static const AkUniqueID PLAY_TIMESTOP = 110520045U;
+        static const AkUniqueID PLAY_TORCHFIRE = 3738480268U;
+        static const AkUniqueID PLAY_WATERFLOW = 518242827U;
+        static const AkUniqueID STOP_CAVEDRIPS = 3406143321U;
+        static const AkUniqueID STOP_FIRECRACKLING = 1334786394U;
+        static const AkUniqueID STOP_TORCHFIRE = 2705271378U;
+        static const AkUniqueID STOP_WATERFLOW = 2135762577U;
+        static const AkUniqueID TIMEBAR = 3776642413U;
+    } // namespace EVENTS
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID SPEED = 640949982U;
+        static const AkUniqueID TIMEBAR = 3776642413U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAIN = 3161908922U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID CAVEAUDIOBUS = 2705837510U;
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
